c00ecd9e91ae6bf6be470c4c7f03c9819345bc84
Ryan Troxler <rtroxler@rednovalabs.com>
2016-02-25 09:24:27 -0600

    Clean SQL queries with some triple """ action

--- lib/tux/db.ex
+++ lib/tux/db.ex
@@ -8,32 +8,55 @@ defmodule Tux.DB do
 
   def fetch_facilities(pid, city) do
     result = query!(pid, """
-    SELECT addressable_id FROM addresses WHERE addresses.addressable_type = $1 AND addresses.city = $2
-    """, ["Facility ", city])
+    SELECT addressable_id FROM addresses
+      WHERE addresses.addressable_type = $1
+      AND addresses.city = $2
+    """, ["Facility", city])
     { :ok, List.flatten(result.rows) }
   end
 
   def fetch_units(pid, facility_ids, unit_length, unit_width) do
-    result = query!(pid, "SELECT id from units where units.length = $1 and units.width = $2 and units.facility_id = ANY ($3)", [unit_length, unit_width, facility_ids])
+    result = query!(pid, """
+    SELECT id from units
+      WHERE units.length = $1
+      AND units.width = $2
+      AND units.facility_id = ANY ($3)
+    """, [unit_length, unit_width, facility_ids])
     { :ok, List.flatten(result.rows)}
   end
 
   def fetch_rental_rates(pid, facility_ids, unit_ids) do
-    #require IEx
-    #IEx.pry
-    result = query!(pid, "SELECT current_rate from ledgers where ledgers.facility_id = ANY ($1) AND ledgers.unit_id = ANY ($2)", [facility_ids, unit_ids])
+    result = query!(pid, """
+    SELECT current_rate from ledgers
+      WHERE ledgers.facility_id = ANY ($1)
+      AND ledgers.unit_id = ANY ($2)
+    """, [facility_ids, unit_ids])
     { :ok, List.flatten(result.rows), result.num_rows }
   end
 
   def fetch_rentals(pid, facility_ids, unit_ids) do
-    result = query!(pid, "SELECT current_rate, moved_in_at, closed_on from ledgers where ledgers.facility_id = ANY ($1) AND ledgers.unit_id = ANY ($2)", [facility_ids, unit_ids])
+    result = query!(pid, """
+    SELECT current_rate, moved_in_at, closed_on from ledgers
+      WHERE ledgers.facility_id = ANY ($1)
+      AND ledgers.unit_id = ANY ($2)
+    """, [facility_ids, unit_ids])
     { :ok, result.rows}
   end
 
   # This is a doozy, and also slow (5-6 seconds each time) make it two queries, maybe?
   # select a.city, a.state, count(l.id) from addresses a join facilities f on f.id = addressable_id and a.addressable_type = 'Facility' join ledgers l on l.facility_id = f.id join units u on l.unit_id = u.id where u.length = 20 and u.width = 20 group by a.city, a.state order by count(l.id) desc limit 10;
   def fetch_popular_cities(pid, unit_length, unit_width) do
-    result = query!(pid, "SELECT a.city, a.state, count(l.id) from addresses a join facilities f on f.id = addressable_id and a.addressable_type = $1 join ledgers l on l.facility_id = f.id join units u on l.unit_id = u.id where u.length = $2 and u.width = $3 group by a.city, a.state order by count(l.id) desc limit 10", ["Facility", unit_length, unit_width])
+    result = query!(pid, """
+    SELECT a.city, a.state, count(l.id) FROM addresses a
+      JOIN facilities f ON f.id = addressable_id AND a.addressable_type = $1
+      JOIN ledgers l ON l.facility_id = f.id
+      JOIN units u ON l.unit_id = u.id
+      WHERE u.length = $2
+      AND u.width = $3
+      GROUP BY a.city, a.state
+      ORDER BY count(l.id) desc
+      LIMIT 10
+    """, ["Facility", unit_length, unit_width])
 
     result.rows
   end
